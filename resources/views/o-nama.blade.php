a9741adafa0db09500b0482fc69173971b1cd4b7
lionwhyte <shiky69@protonmail.com>
2023-11-06 22:57:06 +0100

    about page completed

--- resources/views/o-nama.blade.php
+++ resources/views/o-nama.blade.php
@@ -146,4 +146,108 @@ $breadcrumbs = [
     </div>
 </section>
 
+<section class="px-4 py-20 bg-gray-900 grid sm:grid-cols-2 lg:grid-cols-4 gap-12 my-24 text-white text-center">
+    <div>
+        <div class="text-5xl">26+</div>
+        <div class="text-3xl">Godine iskustva</div>
+    </div>
+    <div>
+        <div class="text-5xl">50+</div>
+        <div class="text-3xl">Timova</div>
+    </div>
+    <div>
+        <div class="text-5xl">2000+</div>
+        <div class="text-3xl">Godišnjih montaža</div>
+    </div>
+    <div>
+        <div class="text-5xl">200+</div>
+        <div class="text-3xl">Tisuća zadovoljnih klijenata</div>
+    </div>
+</section>
+
+<section class="container mx-auto px-4 grid sm:grid-cols-2 lg:grid-cols-3 gap-12 text-gray-700 mb-20">
+        <div class="space-y-6">
+            <img src="{{asset('/images/it-generic-7.jpg')}}">
+            <h2 class="text-2xl text-sky-500">NAŠA MISIJA</h2>
+            <h3 class="text-xl">Naša tehnologija je inovativna. Sada i u budućnosti.</h3>
+            <p>Ono što nas još čini iznimnim je visoka razina inovativnosti: reinvestiramo više od 5 posto našeg prometa u istraživanje i
+                 razvoj ponude proizvoda i usluga. Jamčimo tehnologiju koja je i ostaje inovativna duži niz godina ...</p>
+        </div>
+        <div class="space-y-6">
+            <img src="{{asset('/images/it-generic-8.jpg')}}">
+            <h2 class="text-2xl text-sky-500">NAŠA VIZIJA</h2>
+            <h3 class="text-xl">Optimizirani smo za sve zadane uvijete.</h3>
+            <p>Bilo da se radi o mobilnoj ili stacionarnoj klimatizaciji, za iznajmljivanje ili za prodaju, prijenosnu mjernu tehnologiju za klimu,
+                 kvalitetu i kontrolu emisija ili proizvode koji osiguravaju kvalitetu vode technoprom.hr radi u različitim područjima. 
+                 I svaki od njih je stručnjak u svom području. Rezultat takvoga međusobnog djelovanja je: stručno znanje, visokokvalitetni proizvodi i
+                  sveobuhvatna ponuda pouzdanih usluga - kao individualno ili cjelovito rješenje!</p>
+        </div>
+        <div class="space-y-6">
+            <img src="{{asset('/images/it-generic-9.jpg')}}">
+            <h2 class="text-2xl text-sky-500">KARIJERA</h2>
+            <h3 class="text-xl">Postani dio našeg tima!</h3>
+            <p>Važno je da želiš biti dio našeg tima i da je za tebe, kao i za nas, kupac uvijek na prvom mjestu.</p>
+        </div>
+</section>
+
+<section class="container px-4 space-y-6 my-20">
+    <div class="space-y-1">
+        <img src="" alt="">
+        <h2 class="text-2xl text-sky-500 underline">Klima uređaji prodaja</h2>
+        <p class="font-semibold text-sm">Klima uređaji renomiranih marki po povoljnim cijenama, izdavanje R1 računa,garancija, besplatan uviđaj i procjena montaže.</p>
+    </div>
+    <div class="space-y-1">
+        <img src="" alt="">
+        <h2 class="text-2xl text-sky-500 underline">Centralno grijanje</h2>
+        <p class="font-semibold text-sm">Najpovoljnije centralno - solarno - podno grijanje, prodaja, ugradnja i servis. Nudimo nabavu i montažu svih vrsta i 
+            tipova opreme za centralna grijanja. Naše stručno osoblje, pomoći će Vam izabrati najbolju dizalicu topline, koja odgovara Vašim potrebama.</p>
+    </div>
+    <div class="space-y-1">
+        <img src="" alt="">
+        <h2 class="text-2xl text-sky-500 underline">Termoakumulacijske peći</h2>
+        <p class="font-semibold text-sm">Prodajemo, montiramo, servisiramo po cijeloj Hrvatskoj, Magnohrom termo peći, od 2455 kn + pdv, ovisno o snazi, 
+            za detaljniju i precizniju ponudu molim da nam se direktno obratite putem e-maila, kontakt obrazca ili pozivom na broj 095/123-0003, 
+            luka.technoprom@gmail.com.
+
+        </p>
+    </div>
+    <div class="space-y-1">
+        <img src="" alt="">
+        <h2 class="text-2xl text-sky-500 underline">Montaža klima uređaja</h2>
+        <p class="font-semibold text-sm">Vedran Company - klima centar Technoprom savjetuje. Prilikom izbora klima uređaja treba obratiti pozornost na veličinu i snagu klima uređaja.
+             Svaki klima uređaj mora biti namijenjen za prostoriju u koju ga se namjerava postaviti. Kako bi se izračunao raspon snage u kw, 
+             broj kvadratnih metara prostora se podijeli sa deset za hlađenje i sedam za grijanje. Parametri koji čine razliku između veličine i 
+             snage klima uređaja su sljedeći: položaj prostorije, toplinska izolacija kuće ili stana, električni uređaji u prostoriji, 
+             broj ljudi koji koriste klimatizirane uređaje, te kvaliteta površina izrađenih od stakla u prostoriji. Obavljamo montažu klima uređaja u svim mjestima i 
+             gradovima u RH, sa pedeset (50) radnih timova na raspolaganju raspoređeni po čitavom teritoriju RH - 
+             IZLAZAK NA TEREN VRLO BRZO BEZ DODATNIH TROŠKOVA - 095/123-0003, luka.technoprom@gmail.com.</p>
+    </div>
+    <div class="space-y-1">
+        <img src="" alt="">
+        <h2 class="text-2xl text-sky-500 underline">Dizalice topline</h2>
+        <p class="font-semibold text-sm">Dizalice topline za hlađenje, grijanje, podno grijanje, potrošna topla voda PTV, spajanje na solar Besplatan uviđaj i procjena montaže. 
+            Dizalice topline su idealne za klimu s umjerenim potrebama za grijanjem i hlađenjem, toplinske pumpe nude energetski učinkovitu zamjenu za peći i 
+            klima uređaje. Poput vašeg hladnjaka, toplinske pumpe koriste električnu energiju za premještanje topline iz hladnog u topli prostor, 
+            čineći hladni prostor hladnijim, a topliji prostor toplijim. Tijekom sezone grijanja dizalice topline premještaju toplinu iz hladnog prostora na 
+            otvorenom u vašu toplu kuću, a tijekom sezone hlađenja dizalice topline iz vaše hladne kuće prebacuju toplinu na otvorenom. Budući da premještaju toplinu, 
+            a ne stvaraju toplinu, dizalice topline mogu pružiti ekvivalentno kondicioniranje prostora za samo četvrtinu troškova rada konvencionalnih uređaja 
+            za grijanje ili hlađenje.</p>
+    </div>
+    <div class="space-y-1">
+        <img src="" alt="">
+        <h2 class="text-2xl text-sky-500 underline">Servis klima uređaja</h2>
+        <p class="font-semibold text-sm">Klima uređaji Hrvatska obavlja servisiranje, čišćenje klima uređaja + nabavu, dostavu i ugradnju rezervnih dijelova za cijelu hrvatsku svih vrsta klima uređaja. 
+            Popravak (rad) obavljamo po dogovoru i želji kupca, ovisno u kojem se kupac gradu nalazi. Prodajemo zaštitne navlake za klime ( za vanjske jedinice ) i 
+            daljinske upravljače za sve tipove klima uređaja!</p>
+    </div>
+    <div class="space-y-1">
+        <img src="" alt="">
+        <h2 class="text-2xl text-sky-500 underline">Peći - kotlovi na pelet i drva</h2>
+        <p class="font-semibold text-sm">Peći - kotlovi na pelet i drva je peć koja sagorijeva komprimirano drvo ili pelet na biomasu kako bi stvorila izvor topline za stambene i 
+            ponekad industrijske prostore. Stalnim punjenjem goriva iz spremnika (lijevak) u područje ložišta, on stvara stalni plamen koji zahtjeva malo ili 
+            nimalo fizičkih prilagodbi. Današnji sustavi centralnog grijanja koji rade na drvene pelete kao obnovljivi izvor energije mogu doseći faktor učinkovitosti 
+            veći od 90%.</p>
+    </div>
+</section>
+
 @endsection
